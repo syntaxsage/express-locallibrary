db347c3c97b1b8bfb3c2e2651583251bc56edb67
syntaxsage <thirumoorthisundhar@gmail.com>
2020-12-31 22:07:14 +0530

    First version of application moved into github

---
+++ models/author.js
@@ -0,0 +1,59 @@
+const { DateTime } = require('luxon');
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+    {
+        first_name: {type: String, required: true, maxlength: 100},
+        family_name: {type: String, required: true, maxlength: 100},
+        date_of_birth: {type: Date},
+        date_of_death: {type: Date},
+    }
+);
+
+AuthorSchema
+.virtual('name')
+.get(function() {
+    return this.family_name + ', ' + this.first_name;
+});
+
+AuthorSchema
+.virtual('url')
+.get(function() {
+    return '/catalog/author/' + this._id;
+});
+
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+    return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+    return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual('lifespan').get(function() {
+    var lifetime_string = '';
+    if (this.date_of_birth) {
+      lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+    }
+    lifetime_string += ' - ';
+    if (this.date_of_death) {
+      lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    }
+    return lifetime_string;
+});
+
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function() {
+    return DateTime.fromJSDate(this.date_of_birth).toISODate(); //format 'YYYY-MM-DD'
+});
+  
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function() {
+    return DateTime.fromJSDate(this.date_of_death).toISODate(); //format 'YYYY-MM-DD'
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
